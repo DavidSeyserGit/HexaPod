296bcc197313d24935862e5089e9b226c77b12f5
DavidSeyserGit <96821053+DavidSeyserGit@users.noreply.github.com>
2024-01-29 08:56:58 +0100

    Updated IK function to send appropriate msg

--- src/inverse_kinematics/src/calculation.cpp
+++ src/inverse_kinematics/src/calculation.cpp
@@ -1,30 +1,68 @@
 #include <ros/ros.h>
-#include <std_msgs/Int32MultiArray.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <cmath> 
+
+namespace inverse_kinematics {
+
+    typedef struct {
+        double theta1, theta2, theta3;
+    } JointAngles;
+
+    const double l1 = 1, l2 = 1, l3 = 1; //need to be changed to real values from value (PLACEHOLDER)
+    
+    JointAngles calculateIK(double goal_x, double goal_y, double goal_z) // not goal_pos in worldframe but goal_pos of the leg_endeffector
+    {
+        JointAngles angles;
+        
+        //theta 1
+        angles.theta1 = atan2(goal_y, goal_x);
+
+        //theta 2
+        const float l2l3xy = sqrt(pow(l2, 2) + pow(l3, 2));
+        float beta = atan2(goal_z, l2l3xy);
+        float alpha = acos((pow(l2, 2) + pow(l2l3xy, 2) - pow(l3, 2)) / (2 * l2 * l2l3xy));
+        angles.theta2 = alpha - fabs(beta);
+
+        //theta 3
+        float gamma = acos((pow(l3, 2) + pow(l2l3xy, 2) - pow(l2, 2)) / (2 * l3 * l2l3xy));
+        angles.theta3 = gamma + fabs(beta);
+
+        return angles;
+    }
+}
+
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "invere_kinematics");
     ros::NodeHandle nh;
 
-    ros::Publisher pub = nh.advertise<std_msgs::Int32MultiArray>("goal_positions", 10);
+    ros::Publisher pub = nh.advertise<std_msgs::Float32MultiArray>("goals", 10);
 
     ros::Rate loop_rate(10); 
 
-    while (ros::ok())
+    while (ros::ok()) 
     {
-        std_msgs::Int32MultiArray goal_pos;
-        goal_pos.data.clear(); //safety measure for empty array
+        std_msgs::Float32MultiArray joint_variables;
 
-        //goal_pos.data.push_back(0); //motor 1
-        //goal_pos.data.push_back(123); //motor 2
-        //goal_pos.data.push_back(235); //motor 3
-        //goal_pos.data.push_back(70); //motor 4
+        joint_variables.data.push_back(static_cast<int16_t>(inverse_kinematics::calculateIK(1, 2, 1).theta1)); //goal_pos needs to be changed for every leg
+        joint_variables.data.push_back(static_cast<int16_t>(inverse_kinematics::calculateIK(1, 2, 1).theta2));
+        joint_variables.data.push_back(static_cast<int16_t>(inverse_kinematics::calculateIK(0.1, 0.1, 0.1).theta3));
+        //LEG 2 Inverese Kinematics
+        joint_variables.data.push_back(static_cast<int16_t>(inverse_kinematics::calculateIK(0.1, 0.1, 0.1).theta1)); //goal_pos needs to be changed for every leg
+        joint_variables.data.push_back(static_cast<int16_t>(inverse_kinematics::calculateIK(0.1, 0.1, 0.1).theta2));
+        joint_variables.data.push_back(static_cast<int16_t>(inverse_kinematics::calculateIK(0.1, 0.1, 0.1).theta3));
+        //LEG 3 Inverese Kinematics
+        joint_variables.data.push_back(static_cast<int16_t>(inverse_kinematics::calculateIK(0.1, 0.1, 0.1).theta1)); //goal_pos needs to be changed for every leg
+        joint_variables.data.push_back(static_cast<int16_t>(inverse_kinematics::calculateIK(0.1, 0.1, 0.1).theta2));
+        joint_variables.data.push_back(static_cast<int16_t>(inverse_kinematics::calculateIK(0.1, 0.1, 0.1).theta3));
+        //value between 0 and 360 degrees
 
-        pub.publish(goal_pos); // Publish the message
 
-        ros::spinOnce();
-        loop_rate.sleep(); // Sleep to maintain the loop rate
-    }
+        pub.publish(joint_variables);
+    }   
+    ros::spinOnce();
+    loop_rate.sleep();
 
     return 0;
 }
